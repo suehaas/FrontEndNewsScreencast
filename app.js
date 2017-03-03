1820dca680db7992cb53de24155ad30fca843e8b
sue <sue@srcginc.com>
2017-03-03 14:09:22 -0800

    Add JQuery Like click

---
+++ app.js
@@ -0,0 +1,11 @@
+//------------------------------------------
+//   DOCUMENT READY
+//------------------------------------------*/
+
+$(document).ready(function() {
+  $(".js-like").on("click", function(event) {
+    event.preventDefault();
+
+    $(this).text("Liked!");
+  });
+});
